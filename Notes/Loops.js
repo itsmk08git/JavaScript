ede005e58daa7ff748244f47cb92c2a486684f89
Mohan <mohankoirala201521@gmail.com>
2024-05-20 21:41:13 +0545

    Array_Method_Notes

--- Notes/Loops.js
+++ Notes/Loops.js
@@ -41,3 +41,54 @@
  * 3)str.trim()
  * 4)str.slice(start, end?)
  */
+
+let items = [500,600,700,800,900,1000];
+
+for(let i =0; i < items.length; i++){
+    console.log("items=",items[i]);
+    let offer = items[i] * 0.1;
+    items[i] = items[i] - offer;
+}
+
+console.log(items);
+
+let item = [100,200,300,400];
+
+let final = items.concat(item);
+console.log(final);
+
+/**
+ * Array Methods
+ * 1)shift :
+            Delete form starting
+            Returns a value
+    2)unshift:
+            Add from starting
+            Doesn't return a value
+ */
+
+let marvelHeros = ["thor", "Ironman", "spiderman"]
+
+// marvelHeros = marvelHeros.shift();
+// console.log("Deleted :", marvelHeros);
+
+
+marvelHeros.unshift("C.America");
+console.log("Added :", marvelHeros);
+
+/**
+ * 1)slice(): returns a piece of the array
+ *          Syntax: slice(startldx, endldx)
+ *          
+ * 
+ * 2)splice(): change the original array (add, remove, replace)
+ *              Syntax: splice(startldx, delCount, new)
+ */
+
+let dcHeros = ["Batman", "robin", "Superman", "Cyborg"];
+
+console.log(dcHeros.slice(1,3));  //Index 1st and 2nd will be at console
+
+let arr = [1, 2, 3, 4, 5, 6];
+arr.splice(2, 3, 12, 34, 45);
+console.log(arr);

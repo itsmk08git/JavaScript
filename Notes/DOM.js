886019ea47bd75cfce34330df3c3ea95ecc480a5
Mohan <mohankoirala201521@gmail.com>
2024-05-28 22:14:23 +0545

    DOM_Notes

---
+++ Notes/DOM.js
@@ -0,0 +1,20 @@
+/**
+ * DOM : Document Object Model
+ * 
+*/
+
+/**
+ * Window Object:
+ * The window object represents an open window in a browser.
+ * It is browser's object and automatically created by browser.
+ * It's a global object.
+ */
+
+/**
+ * DOM Manipulation
+ * 1)selecting with id: document.getElementById("myId")
+ * 
+ * 2)selecting with class: document.getElementByClassName("myClass")
+ * 
+ * 3)selecting with tag: document.getElementByTagName("p")
+ */

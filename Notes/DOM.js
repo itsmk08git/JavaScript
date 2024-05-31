136b9aeab9baa8fdbfdfc60d3747a81176621a79
Mohan <mohankoirala201521@gmail.com>
2024-05-31 21:03:14 +0545

    Project

--- Notes/DOM.js
+++ Notes/DOM.js
@@ -17,4 +17,29 @@
  * 2)selecting with class: document.getElementByClassName("myClass")
  * 
  * 3)selecting with tag: document.getElementByTagName("p")
+ * 
+ * Query Selectors
+ * 1)document.querySelector("myId/myClass/tag") //returns first element
+ * 
+ * 2)document.querySelectorAll("myId/myClass/tag")  //returns a NodeList
+ * 
+ * 
+ * =>PROPERTIES:
+ * tagName: returns tag for element nodes
+ * innerText: returns the text content of the element and all its children
+ * innerHTML: returns the plain text to or HTML contents in the elements
+ * textContent: returns textual content even for hidden elements
+ * 
  */
+
+ // console.log(document.querySelector('button').innerHTML);  
+        //     //Every HTML has a property .innerHTML
+
+        // document.querySelector('button').innerHTML = 'Welcome'
+
+        // const buttonElement= document.querySelector(".js-button");
+        // console.log(buttonElement);
+        /* document.body.innerHTML = 'Hello';
+         document.title = 'Rock Paper'
+         document.body.innerHTML = '<button>Good Job</button>';
+        */
